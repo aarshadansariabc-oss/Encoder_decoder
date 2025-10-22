804b00886cd060cc3ae260e31324a6be197ec95e
Arshad Ansari <aarshadansariabc@gmail.com>
2025-10-22 17:50:58 +0530

    Making it User Friendly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-
-
+import logo
 Alphabets = [
 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
@@ -10,17 +9,26 @@ def ceaser(Start_text, Shift_amount, ciper_direction):
     Text_res = ""
     if ciper_direction == "decode":
             Shift_amount *= -1
-            # 5 * -1  = -5
     for letter in Start_text:
-        position = Alphabets.index(letter)
-        
-        new_positon = position + Shift_amount
-        Text_res += Alphabets[new_positon]
+        if letter in Alphabets:
+            position = Alphabets.index(letter)
+            new_positon = position + Shift_amount
+            Text_res += Alphabets[new_positon]
+        else:
+             Text_res +=letter
+            
     print(f"The {ciper_direction} text is {Text_res}")  
             
+print(logo.l)
+flag = True
+while flag:
+    direction = input("""Type 'encode' to encrypt, Type 'decode' to Decrypt : """)
+    text = input("Type your Message : ").lower()
+    shift = int(input("Type Your Shift Number : "))
 
-direction = input("""Type 'encode' to encrypt, Type 'decode' to Decrypt : """)
-text = input("Type your Message : ").lower()
-shift = int(input("Type Your Shift Number : "))
-
-ceaser(Start_text= text, Shift_amount= shift, ciper_direction= direction)
+    shift %= 26
+    ceaser(Start_text= text, Shift_amount= shift, ciper_direction= direction)
+    r = input("Do you want to Run is again 'Yes' or 'No' : " ).lower()
+    if r == "no":
+         flag = False
+         print("GoodBye !")
